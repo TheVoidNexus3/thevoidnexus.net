9a398752bb92fd3ac7c1ed8a554d47e81dd1cd13
TheVoidNexus <150041338+TheVoidNexus@users.noreply.github.com>
2024-01-31 08:21:46 +0100

    v0.6
    Bug Fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Created by TheVoidNexus on 31.01.2024
 
 function redirect() {
-  window.location.href = "thevoidnexus.github.io/database";
+  window.location.href = "https://thevoidnexus.github.io/database/";
 }
