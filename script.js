0eb0b0e1021c8dfc7bb4ed1209d54c1584506613
TheVoidNexus <eric.berger@revylo.de>
2024-02-07 13:29:10 +0000

    v2.3

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // Created by TheVoidNexus on 31.01.2024 | Updated: 07.02.2024
 
+let savedMoney = "savedMoney";
+let savedUpgrade = "savedUpgrade";
+let savedMPS = "savedMPS";
+
 function redirect() {
   window.location.href = "https://thevoidnexus.github.io/database/";
 }
+
 function redirect2() {
   window.location.href = "https://thevoidnexus.github.io/calculator/";
 }
 
-//
-
-let money = localStorage.getItem(savedMoney);
-if(money == null) {money = 0};
-let upgradeMoney = localStorage.getItem(savedUpgrade);
-if(upgradeMoney == null) {upgradeMoney = 10};
-let MPS = localStorage.getItem(savedMPS);
-if(MPS == null) {MPS = 0}
+let money = parseInt(localStorage.getItem(savedMoney)) || 0;
+let upgradeMoney = parseInt(localStorage.getItem(savedUpgrade)) || 10;
+let MPS = parseInt(localStorage.getItem(savedMPS)) || 0;
 
 function update() {
-  let Info = document.getElementById(`Info`)
-  Info.innerHTML = `You currently have $${money}.`
+  let Info = document.getElementById(`Info`);
+  Info.innerHTML = `You currently have $${money}.`;
 }
 
 function clicker() {
-money += MPS;
-update();
-localStorage.setItem(savedMoney, money);
+  money += MPS;
+  update();
+  localStorage.setItem(savedMoney, money);
 }
 
 function clickerUpgrade() {
-  if(money >= upgradeMoney) {
+  if (money >= upgradeMoney) {
     money -= upgradeMoney;
     MPS *= 2;
     upgradeMoney *= 1.5;
@@ -41,6 +41,8 @@ function clickerUpgrade() {
   }
 }
 
-setInterval(function() {
+setInterval(function () {
   money += MPS;
+  update();
+  localStorage.setItem(savedMoney, money);
 }, 1000);
