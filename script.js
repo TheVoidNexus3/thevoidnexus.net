0a4db1ef2c24f345df61a5b0af7510e5b31cdab0
TheVoidNexus <eric.berger@revylo.de>
2024-02-07 14:05:47 +0000

    v2.8

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 let savedMoney = "savedMoney";
 let savedUpgrade = "savedUpgrade";
+let savedUpgrade2 = "savedUpgrade2"
 let savedMPS = "savedMPS";
+let savedMPC = "savedMPC"
 
 function redirect() {
   window.location.href = "https://thevoidnexus.github.io/database/";
@@ -15,19 +17,21 @@ function redirect2() {
 let money = parseInt(localStorage.getItem(savedMoney)) || 0;
 let upgradeMoney = parseInt(localStorage.getItem(savedUpgrade)) || 10;
 let MPS = parseInt(localStorage.getItem(savedMPS)) || 0;
+let MPC = parseInt(localStorage.getItem(savedMPC)) || 1;
 
 function update() {
   let Info = document.getElementById(`Info`);
   let Button1 = document.getElementById(`Button1`)
   let Button2 = document.getElementById(`Button2`)
+  let Button3 = document.getElementById(`Button3`)
   Info.innerHTML = `You currently have $${money}. Money per second: $${MPS}`;
-  Button1.innerHTML = `Earn Money<br>Money per click: $${MPS}`
-  Button2.innerHTML = `Upgrade<br>Cost: $${upgradeMoney}`
+  Button1.innerHTML = `Earn Money<br>Money per click: $${MPC}`
+  Button2.innerHTML = `Upgrade Money per Second<br>Cost: $${upgradeMoney}`
+  Button3.innerHTML = `Upgrade Money per Click<br>Cost: $${upgradeMoney2}`
 }
 
 function clicker() {
-  if(MPS != 0) {
-   money += MPS} else {money += 1}
+  money += MPC;
   update();
   localStorage.setItem(savedMoney, money);
 }
@@ -39,12 +43,16 @@ function clickerUpgrade() {
     upgradeMoney *= 2.25;
     upgradeMoney = Math.round(upgradeMoney);
     money = Math.round(money);
-    
-    localStorage.setItem(savedMoney, money);
-    localStorage.setItem(savedUpgrade, upgradeMoney);
-    localStorage.setItem(savedMPS, MPS);
+  }
+}
 
-    alert(`Successfully upgraded!`);
+function clickerUpgrade2() {
+  if(money >= upgradeMoney2) {
+    money -= upgradeMoney2;
+    MPC *= 2;
+    upgradeMoney2 *= 2.25;
+    upgradeMoney2 = Math.round(upgradeMoney2);
+    money = Math.round(money);
   }
 }
 
@@ -52,4 +60,8 @@ setInterval(function () {
   money += MPS;
   update();
   localStorage.setItem(savedMoney, money);
+  localStorage.setItem(savedMPS, MPS);
+  localStorage.setItem(savedMPC, MPC);
+  localStorage.setItem(savedUpgrade, upgradeMoney);
+  localStorage.setItem(savedUpgrade2, upgradeMoney2);
 }, 1000);
