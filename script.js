dcbbe2af1f13f8468ada83d08d05da833778ab98
TheVoidNexus <150041338+TheVoidNexus@users.noreply.github.com>
2024-02-07 10:44:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Created by TheVoidNexus on 31.01.2024
+// Created by TheVoidNexus on 31.01.2024 | Updated: 07.02.2024
 
 function redirect() {
   window.location.href = "https://thevoidnexus.github.io/database/";
 }
 function redirect2() {
   window.location.href = "https://thevoidnexus.github.io/calculator/";
+}
+
+//
+let money = 0
+
+window.onload = loaded(); {
+money = localStorage.getItem(money);
+update();
+}
+
+function update() {
+  let Info = document.getElementById(`Info`)
+  Info.innerHTML = `You currently have $${money}.`
+}
+
+function clicker() {
+money += 1;
+update();
+localStorage.setItem(money, money);
 }
