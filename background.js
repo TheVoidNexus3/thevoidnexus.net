210511982b41d14cda7ee24a86da55983887d2bd
TheVoidNexus <eric.berger@revylo.de>
2024-03-25 11:10:27 +0000

    v5.3

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function createDot() {
     const dot = document.createElement('div');
     dot.classList.add('dot');
     dot.style.top = `${50 + Math.random() * (window.innerHeight - 50)}px`;
-    dot.style.animationDuration = `${15 + Math.random() * 5}s`;
+    dot.style.animationDuration = `${30 + Math.random() * 5}s`;
 
     const hue = Math.random() < 0.5 ? Math.floor(300 + Math.random() * 31) : Math.floor(270 + Math.random() * 31);
     dot.style.backgroundColor = `hsl(${hue}, 100%, 70%)`;
