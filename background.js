8fb6cc9536361d1a7c7694ffb358c4d8bf663c7e
TheVoidNexus <eric.berger@revylo.de>
2024-03-25 10:52:00 +0000

    v5.1

--- background.js
+++ background.js
@@ -3,12 +3,29 @@ function createDot() {
     dot.classList.add('dot');
     dot.style.top = `${50 + Math.random() * (window.innerHeight - 50)}px`;
     dot.style.animationDuration = `${15 + Math.random() * 5}s`;
-    document.querySelector('.dynamic-background').appendChild(dot);
 
     const hue = Math.random() < 0.5 ? Math.floor(300 + Math.random() * 31) : Math.floor(270 + Math.random() * 31);
-        dot.style.backgroundColor = `hsl(${hue}, 100%, 70%)`;
+    dot.style.backgroundColor = `hsl(${hue}, 100%, 70%)`;
 
-        dotContainer.appendChild(dot);
+    document.querySelector('.dynamic-background').appendChild(dot);
 }
 
-setInterval(createDot, 750);
+let dotInterval;
+
+function startDotCreation() {
+    dotInterval = setInterval(createDot, 750);
+}
+
+function stopDotCreation() {
+    clearInterval(dotInterval);
+}
+
+document.addEventListener('visibilitychange', function() {
+    if (document.visibilityState === 'hidden') {
+        stopDotCreation();
+    } else {
+        startDotCreation();
+    }
+});
+
+startDotCreation();
