e25442ffe911d1e383780abf3f99c283edd885fc
TheVoidNexus <eric.berger@revylo.de>
2024-03-26 07:31:40 +0000

    - Added more money abbrevations - Adjusted upgrade effects

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Created by TheVoidNexus on 31.01.2024 | Updated: 25.03.2024
+// Created by TheVoidNexus on 31.01.2024 | Updated: 26.03.2024
 
 const MILLISECONDS_PER_SECOND = 1000;
 const UPDATE_INTERVAL = 1000;
@@ -36,48 +36,131 @@ function moneyRounder(thisMoney) {
   let suffix = "";
   let roundedMoney = thisMoney;
 
-  if (thisMoney >= 1000) {
-    if (thisMoney < 1000000) {
-      roundedMoney = (thisMoney / 1000).toFixed(2);
-      suffix = "K";
-    }
+  if (thisMoney >= 10**3) {
+    roundedMoney = (thisMoney / 10**3).toFixed(0);
+    suffix = "K";
   }
-  if (thisMoney >= 1000000) {
-    if (thisMoney < 1000000000) {
-      roundedMoney = (thisMoney / 1000000).toFixed(2);
-      suffix = "M";
-    }
+  if (thisMoney >= 10**6) {
+    roundedMoney = (thisMoney / 10**6).toFixed(0);
+    suffix = "M";
   }
-  if (thisMoney >= 1000000000) {
-    if (thisMoney < 1000000000000) {
-      roundedMoney = (thisMoney / 1000000000).toFixed(2);
-      suffix = "B";
-    }
+  if (thisMoney >= 10**9) {
+    roundedMoney = (thisMoney / 10**9).toFixed(0);
+    suffix = "B";
   }
-  if (thisMoney >= 1000000000000) {
-    roundedMoney = (thisMoney / 1000000000000).toFixed(2);
+  if (thisMoney >= 10**12) {
+    roundedMoney = (thisMoney / 10**12).toFixed(0);
     suffix = "T";
   }
   if (thisMoney >= 10**15) {
-    roundedMoney = (thisMoney / 10**15).toFixed(2);
+    roundedMoney = (thisMoney / 10**15).toFixed(0);
     suffix = "Qa";
   }
   if (thisMoney >= 10**18) {
-    roundedMoney = (thisMoney / 10**18).toFixed(2);
+    roundedMoney = (thisMoney / 10**18).toFixed(0);
     suffix = "Qi";
   }
   if (thisMoney >= 10**21) {
-    roundedMoney = (thisMoney / 10**21).toFixed(2);
+    roundedMoney = (thisMoney / 10**21).toFixed(0);
     suffix = "Sx";
   }
   if (thisMoney >= 10**24) {
-    roundedMoney = (thisMoney / 10**24).toFixed(2);
+    roundedMoney = (thisMoney / 10**24).toFixed(0);
     suffix = "Sp";
   }
   if (thisMoney >= 10**27) {
-    roundedMoney = (thisMoney / 10**27).toFixed(2);
+    roundedMoney = (thisMoney / 10**27).toFixed(0);
     suffix = "Oc";
   }
+  if (thisMoney >= 10**30) {
+    roundedMoney = (thisMoney / 10**30).toFixed(0);
+    suffix = "No";
+  }
+  if (thisMoney >= 10**33) {
+    roundedMoney = (thisMoney / 10**33).toFixed(0);
+    suffix = "Dc";
+  }
+  if (thisMoney >= 10**36) {
+    roundedMoney = (thisMoney / 10**36).toFixed(0);
+    suffix = "Udc";
+  }
+  if (thisMoney >= 10**39) {
+    roundedMoney = (thisMoney / 10**39).toFixed(0);
+    suffix = "Ddc";
+  }
+  if (thisMoney >= 10**42) {
+    roundedMoney = (thisMoney / 10**42).toFixed(0);
+    suffix = "Tdc";
+  }
+  if (thisMoney >= 10**45) {
+    roundedMoney = (thisMoney / 10**45).toFixed(0);
+    suffix = "Qt";
+  }
+  if (thisMoney >= 10**48) {
+    roundedMoney = (thisMoney / 10**48).toFixed(0);
+    suffix = "Qd";
+  }
+  if (thisMoney >= 10**51) {
+    roundedMoney = (thisMoney / 10**51).toFixed(0);
+    suffix = "Sd";
+  }
+  if (thisMoney >= 10**54) {
+    roundedMoney = (thisMoney / 10**54).toFixed(0);
+    suffix = "St";
+  }
+  if (thisMoney >= 10**57) {
+    roundedMoney = (thisMoney / 10**57).toFixed(0);
+    suffix = "Ocdc";
+  }
+  if (thisMoney >= 10**60) {
+    roundedMoney = (thisMoney / 10**60).toFixed(0);
+    suffix = "Nmdc";
+  }
+  if (thisMoney >= 10**63) {
+    roundedMoney = (thisMoney / 10**63).toFixed(0);
+    suffix = "Vg";
+  }
+  if (thisMoney >= 10**66) {
+    roundedMoney = (thisMoney / 10**66).toFixed(0);
+    suffix = "Uvg";
+  }
+  if (thisMoney >= 10**69) {
+    roundedMoney = (thisMoney / 10**69).toFixed(0);
+    suffix = "Dvg";
+  }
+  if (thisMoney >= 10**72) {
+    roundedMoney = (thisMoney / 10**72).toFixed(0);
+    suffix = "Tvg";
+  }
+  if (thisMoney >= 10**75) {
+    roundedMoney = (thisMoney / 10**75).toFixed(0);
+    suffix = "Qav";
+  }
+  if (thisMoney >= 10**78) {
+    roundedMoney = (thisMoney / 10**78).toFixed(0);
+    suffix = "Qvg";
+  }
+  if (thisMoney >= 10**81) {
+    roundedMoney = (thisMoney / 10**81).toFixed(0);
+    suffix = "Svg";
+  }
+  if (thisMoney >= 10**84) {
+    roundedMoney = (thisMoney / 10**84).toFixed(0);
+    suffix = "Spv";
+  }
+  if (thisMoney >= 10**87) {
+    roundedMoney = (thisMoney / 10**87).toFixed(0);
+    suffix = "Ovg";
+  }
+  if (thisMoney >= 10**90) {
+    roundedMoney = (thisMoney / 10**90).toFixed(0);
+    suffix = "Nvg";
+  }
+  if (thisMoney >= 10**93) {
+    roundedMoney = (thisMoney / 10**93).toFixed(0);
+    suffix = "Tg";
+  }
+
 
   return {
     amount: roundedMoney,
@@ -123,7 +206,7 @@ function clicker() {
 function clickerUpgrade() {
   if (money >= upgradeMoney) {
     money -= upgradeMoney;
-    if(MPS != 0) {MPS *= 1.5} else {MPS += 1}
+    if(MPS == 0) {MPS += 1} else {MPS += 1.45}
     upgradeMoney *= 1.5;
     MPS = Math.round(MPS);
     upgradeMoney = Math.round(upgradeMoney);
@@ -134,7 +217,7 @@ function clickerUpgrade() {
 function clickerUpgrade2() {
   if (money >= upgradeMoney2) {
     money -= upgradeMoney2;
-    MPC *= 1.5;
+    if(MPC != 1) {MPC *= 1.45} else {MPC += 1}
     upgradeMoney2 *= 1.5;
     upgradeMoney2 = Math.round(upgradeMoney2);
     MPC = Math.round(MPC);
